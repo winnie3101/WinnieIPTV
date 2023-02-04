cc7a73ed4b766eabcd6c54243e5f5cc5d736a464
위니 Winnie <62056141+winnie3101@users.noreply.github.com>
2023-02-04 18:33:09 +0700

    Update wavve.php

--- livestreams/kr_live/wavve.php
+++ livestreams/kr_live/wavve.php
@@ -29,17 +29,14 @@
 	// iHQ Delicious guys (iHQ 맛있는녀석들) = C5202
 	// KBS 2 Days & 1 Night (KBS 1박 2일) = K15
 	
-	
 
 	$id = $_GET['id'];
 	$url = "https://apis.wavve.com/fz/streaming?device=pc&partner=pooq&apikey=E5F3E0D30947AA5440556471321BB6D9&credential=none&service=wavve&pooqzone=none&region=kor&drm=none&targetage=all&contentid={$id}&hdr=hdr&videocodec=avc&audiocodec=aac&issurround=y&format=normal&withinsubtitle=y&contenttype=live&action=hls&protocol=hls&quality=1080p&deviceModelId=Windows%2010&guid=8ff1950c-a0a8-11ed-8cd0-3675fb283093&lastplayid=none&authtype=cookie&isabr=y&ishevc=y";
 	$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_PROXY, '220.87.121.155:80');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_HEADER, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
    
 		$data = curl_exec($ch);
