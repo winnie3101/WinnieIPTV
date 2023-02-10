4692d554ee7d13f832a2c3ae7487587c34a18658
위니 Winnie <62056141+winnie3101@users.noreply.github.com>
2023-02-10 11:11:13 +0700

    changed target to 3Plus API

--- livestreams/th_live/ch3.php
+++ livestreams/th_live/ch3.php
@@ -8,16 +8,12 @@
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
         $data = curl_exec($ch);
         curl_close($ch);
+		
+		$json = json_decode($data);
+		$PlayURL = $json->result->streamUrl;
 
-	preg_match('/streamUrl":"(.*?)"/i', $data, $matches);
+	$matches = str_replace('playlist.m3u8', 'playlist_720p/index.m3u8', $matches);
 	
-	$matches = str_replace('\/live\/', '/live/', $matches);
-	$matches = str_replace('&x_ark_path_prefix=\/live\/', '&x_ark_path_prefix=/live/', $matches);
-	$matches = str_replace('&x_ark_path_prefix=/live/', '&x_ark_path_prefix=%2Flive%2F', $matches);
-	#$matches = str_replace('playlist.m3u8', 'playlist_720p/index.m3u8', $matches);
-	
-    $PlayURL = $matches[1];
+    #$PlayURL = $matches[1];
     header("location:$PlayURL");
-   
-   
 ?>
