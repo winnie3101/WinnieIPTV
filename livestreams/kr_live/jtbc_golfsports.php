9f1d4b3bf0e858f76f6554fb737ee4a109c2182d
위니 Winnie <62056141+winnie3101@users.noreply.github.com>
2023-02-16 11:15:52 +0700

    Updated for add Proxy 😉

--- livestreams/kr_live/jtbc_golfsports.php
+++ livestreams/kr_live/jtbc_golfsports.php
@@ -1,15 +1,20 @@
 <?php
     $bstrURL = "http://jtbcgolfnsports.joins.com/inc/main_ovenplayer_onair.asp";
-	$proxy = '220.87.121.155:80';
+    $proxy = "115.144.102.39";
+	$proxyPort = "10080";
     $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $bstrURL);
 		curl_setopt($ch, CURLOPT_PROXY, $proxy);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
-        $data = curl_exec($ch);
-        curl_close($ch);
+		curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
+		#curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+		curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTPS');
+		curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
+		$data = curl_exec($ch);
+		curl_close($ch);
 	
     preg_match('/streamingURL: \'(.*?)\'/i', $data, $matches);
 
--- livestreams/kr_live/sbs.php
+++ livestreams/kr_live/sbs.php
@@ -18,19 +18,22 @@
     */
    
     $id = $_GET['id']; //S01
-	$bstrURL = "http://dev.apis.sbs.co.kr/play-api/2.0/onair/channel/$id?v_type=2&platform=pcweb&protocol=hls&ssl=N&rscuse=&jwt-token=N&rnd=616";
-	#$proxy = '220.87.121.155:80';
+	$bstrURL = "https://apis.sbs.co.kr/play-api/2.0/onair/channel/$id?v_type=2&platform=pcweb&protocol=hls&ssl=N&rscuse=&jwt-token=&rnd=683";
+	$proxy = "115.144.102.39";
+	$proxyPort = "10080";
     $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $bstrURL);
-		#curl_setopt($ch, CURLOPT_PROXY, $proxy);
-		#curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-		#curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
-        $data = curl_exec($ch);
-        curl_close($ch);
+		curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
+		#curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+		curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTPS');
+		curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
+		$data = curl_exec($ch);
+		curl_close($ch);
    
     $json = json_decode($data);
     //onair.source.mediasource.mediaurl
