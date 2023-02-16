654909c00fb48b12d92db41e8dcbe1a954f10076
위니 Winnie <62056141+winnie3101@users.noreply.github.com>
2023-02-16 11:22:11 +0700

    Add files via upload

--- livestreams/kr_live/jtbc_golfsports.php
+++ livestreams/kr_live/jtbc_golfsports.php
@@ -1,17 +1,15 @@
 <?php
     $bstrURL = "http://jtbcgolfnsports.joins.com/inc/main_ovenplayer_onair.asp";
-    $proxy = "115.144.102.39";
-	$proxyPort = "10080";
+    #$proxy = "59.11.153.88";
+	#$proxyPort = "80";
     $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $bstrURL);
-		curl_setopt($ch, CURLOPT_PROXY, $proxy);
-		curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
+		#curl_setopt($ch, CURLOPT_PROXY, $proxy);
+		#curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
 		#curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-		curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTPS');
-		curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+		#curl_setopt($ch, CURLOPT_PROXYTYPE, 'HTTP');
+		#curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36');
 		$data = curl_exec($ch);
 		curl_close($ch);
